6f813f3c3f6af64957f760407853a325543e7eac
Marco Wang <marcowang01@gmail.com>
2022-10-30 14:23:34 -0500

    frontend + dot env

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,11 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://www.nublockchaingroup.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            NU Blockchain
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -21,23 +21,20 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://www.nublockchaingroup.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
+                <h1>NU Blockchain Club Starter Kit</h1>
                 <a
                   className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://www.nublockchaingroup.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  The Blockchain Club is <u><b>great! </b></u>
                 </a>
               </div>
             </main>
